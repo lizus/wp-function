bc7a5947b2ddcdc5cea70b3a048e5b8bd36a2add
lizus <lizus.com@gmail.com>
2024-03-08 10:39:20 +0800

    20240308

--- src/function/order_clear_cache.php
+++ src/function/order_clear_cache.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace LizusFunction;
 
 
@@ -7,6 +8,12 @@ namespace LizusFunction;
  * 设置url的query中clear_cache=ok来要求清理对应页面的redis缓存
  * @return boolean
  */
-function order_clear_cache(){
-    return (isset($_GET['clear_cache']) && $_GET['clear_cache']=='ok');
+function order_clear_cache()
+{
+    /**
+     * 如果不是编辑以上权限，就不允许清理缓存
+     */
+    if (!current_user_can('manage_categories')) return false;
+
+    return (isset($_GET['clear_cache']) && $_GET['clear_cache'] == 'ok');
 }
